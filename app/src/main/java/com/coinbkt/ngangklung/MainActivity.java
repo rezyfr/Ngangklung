d193b299b5ea5d01c21eea655b1b066ad71f3524
rezyfr <frotylatz@gmail.com>
2019-04-18 14:39:31 +0700

    homepage, about, splashcreen

---
+++ app/src/main/java/com/coinbkt/ngangklung/MainActivity.java
@@ -0,0 +1,28 @@
+package com.coinbkt.ngangklung;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btnAbout)
+    ImageView btnAbout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        btnAbout.setOnClickListener((View v) -> {
+            startActivity(new Intent(MainActivity.this, AboutActivity.class));
+        });
+    }
+}
---
+++ app/src/main/java/com/coinbkt/ngangklung/SplashScreen.java
@@ -0,0 +1,54 @@
+package com.coinbkt.ngangklung;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    protected int _splashTime = 2500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        StartAnimations();
+
+    }
+
+    private void StartAnimations() {
+
+        Animation loadAnimation1 = AnimationUtils.loadAnimation(this, R.anim.translate_anim);
+        loadAnimation1.reset();
+        ImageView iv1 = findViewById(R.id.ivSplashscreen);
+        iv1.clearAnimation();
+        iv1.startAnimation(loadAnimation1);
+
+        Thread splashThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    while (waited < _splashTime) {
+                        sleep(100);
+                        waited += 100;
+                    }
+                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent);
+                    SplashScreen.this.finish();
+                } catch (InterruptedException e) {
+
+                } finally {
+                    SplashScreen.this.finish();
+                }
+
+            }
+        };
+        splashThread.start();
+
+    }
+}
